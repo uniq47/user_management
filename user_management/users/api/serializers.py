11f7712899aa8c9ee16885868388c11a1038bcf3
uniq47 <usapkota47@gmail.com>
2024-04-28 18:42:20 -0700

    added readme

--- user_management/users/api/serializers.py
+++ user_management/users/api/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from ..models import User
 
+# serialziers converts complex queryset and model instace into python data type.
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = '__all__'  # list of fields
--- user_management/users/api/views.py
+++ user_management/users/api/views.py
@@ -2,10 +2,12 @@ from rest_framework import generics
 from ..models import User
 from .serializers import UserSerializer
 
+#to get and crate user
 class UserListCreate(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+#to get delete and update specific user
 class UserRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
